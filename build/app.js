4bf0a5351194e3476e2b98a418c013f669419227
freegyes <freegyes@gmail.com>
2017-05-06 18:27:45 +0200

    added myjson as a backend

--- build/app.js
+++ build/app.js
@@ -1,24 +1,11 @@
-Parse.initialize("hM0HusTC50hHwF9ToMM9BYMvb9jA0sPoo8ucQzZp", "JYPke4jXEv0kl4JpmBV9bybpTbzqsHYrfqpVQsui");
+var firstLine = $('#firstLine');
+var author = $('#author');
+var book = $('#book');
 
-    var firstLine = $('#firstLine');
-    var author = $('#author');
-    var book = $('#book');
+$.getJSON( "https://api.myjson.com/bins/125w29", function( data ) {
+  var rnd = Math.floor(Math.random() * (data.length));
 
-    var FirstLine = Parse.Object.extend("FirstLine");
-    
-    var query = new Parse.Query(FirstLine);
-    query.find({
-      success: function(results) {
-
-        var rnd = Math.floor(Math.random() * (results.length));
-
-        firstLine.append(results[rnd].attributes.firstline);
-        book.append(results[rnd].attributes.book);
-        author.append("&mdash; " + results[rnd].attributes.author);
-      },
-
-      error: function(error) {
-        firstline.append("Something went wrong.");
-        book.append("Try again.");
-      }
-    });
+  firstLine.append(data[rnd].firstline);
+  book.append(data[rnd].book);
+  author.append("&mdash; " + data[rnd].author);
+});
