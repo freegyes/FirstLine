44c6dfd15062a147ca9897b9a5ab3b323c96f92f
Adam Freisinger <freegyes@gmail.com>
2020-08-14 12:24:59 +0200

    replace myjson with aws bucket

--- build/app.js
+++ build/app.js
@@ -2,7 +2,7 @@ var firstLine = $('#firstLine');
 var author = $('#author');
 var book = $('#book');
 
-$.getJSON( "https://api.myjson.com/bins/125w29", function( data ) {
+$.getJSON( "http://first-moustache-unicorn.s3-website.eu-central-1.amazonaws.com/firstLines.json", function( data ) {
   var rnd = Math.floor(Math.random() * (data.length));
 
   firstLine.append(data[rnd].firstline);
